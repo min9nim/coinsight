0aada8815e41201935a7a59523ae4689526fbdcd
keating <min9nim>
2021-11-30 19:49:45 +0900

    add market options

--- src/pages/my-orders.js
+++ src/pages/my-orders.js
@@ -12,13 +12,14 @@ import {
 import { parseSearchParams, toComma, oneOf } from '@madup-inc/utils'
 import axios from 'axios'
 import moment from 'moment'
-import {useLoading} from 'react-hook-loading'
-
+import { useLoading } from 'react-hook-loading'
 
 moment.locale('en')
 
 export default () => {
+  const [market, setMarket] = useState('KRW-BTC')
   const [data, setData] = useState([])
+  const [options, setOptions] = useState([])
   const [loading, setLoading] = useLoading()
 
   const [accessKey, setAccessKey] = useState(
@@ -37,8 +38,26 @@ export default () => {
   const loadData = () => {
     setLoading(true)
     axios
+      .get(`https://buy-btc.vercel.app/api/my-accounts`, {
+        params: { accessKey, secretKey },
+      })
+      .then(result => {
+        setOptions(
+          result.data.map(item => item.unit_currency + '-' + item.currency),
+        )
+      })
+      .catch(err => {
+        alert(err.message)
+        localStorage.clear()
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+    setLoading(true)
+    axios
       .get(`https://buy-btc.vercel.app/api/my-orders`, {
-        params: { accessKey, secretKey, orderBy: 'asc', market: 'KRW-BTC' },
+        params: { accessKey, secretKey, orderBy: 'asc', market},
       })
       .then(result => {
         setData(
@@ -58,18 +77,17 @@ export default () => {
       })
   }
 
-
   useEffect(() => {
     if (accessKey.length !== 40 || secretKey.length !== 40) {
       return
     }
 
     loadData()
-  }, [accessKey, secretKey])
+  }, [accessKey, secretKey, market])
 
   console.log('data', data)
 
-  if(loading){
+  if (loading) {
     return null
   }
 
@@ -78,50 +96,61 @@ export default () => {
       [
         data.length > 0,
         () => (
-          <ResponsiveContainer width="100%" height="100%">
-            <ScatterChart
-              margin={{
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: 20,
-              }}
-            >
-              <CartesianGrid />
-              <XAxis
-                type="number"
-                dataKey="x"
-                name="date"
-                angle={10}
-                tickFormatter={value => moment(value).format('YY/MM/DD')}
-                domain={['auto', 'auto']}
-              />
-              <YAxis
-                type="number"
-                width={80}
-                dataKey="y"
-                name="price"
-                domain={['auto', 'auto']}
-                tickFormatter={value => value > 1000000 ? toComma(value/1000000) + '백만' : toComma(value)}
-              />
-              <ZAxis
-                type="number"
-                dataKey="z"
-                domain={['auto', 'auto']}
-                range={[50, 500]}
-                name="volume"
-              />
-              <Tooltip
-                cursor={{ strokeDasharray: '3 3' }}
-                formatter={(value, type) =>
-                  type === 'date'
-                    ? moment(value).format('MM/DD HH:mm')
-                    : toComma(value)
-                }
-              />
-              <Scatter name="A school" data={data} fill="#8884d8" />
-            </ScatterChart>
-          </ResponsiveContainer>
+          <div style={{height: '100vh'}}>
+            <select value={market} name="market" onChange={e => {
+              setMarket(e.target.value)
+            }}>
+              {options.map(item => <option value={item}>{item}</option>)}
+            </select>
+            <ResponsiveContainer width="100%" height="100%">
+              <ScatterChart
+                margin={{
+                  top: 20,
+                  right: 20,
+                  bottom: 20,
+                  left: 20,
+                }}
+              >
+                <CartesianGrid />
+                <XAxis
+                  type="number"
+                  dataKey="x"
+                  name="date"
+                  angle={10}
+                  tickFormatter={value => moment(value).format('YY/MM/DD')}
+                  domain={['auto', 'auto']}
+                />
+                <YAxis
+                  type="number"
+                  width={80}
+                  dataKey="y"
+                  name="price"
+                  domain={['auto', 'auto']}
+                  tickFormatter={value =>
+                    value > 1000000
+                      ? toComma(value / 1000000) + '백만'
+                      : toComma(value)
+                  }
+                />
+                <ZAxis
+                  type="number"
+                  dataKey="z"
+                  domain={['auto', 'auto']}
+                  range={[50, 500]}
+                  name="volume"
+                />
+                <Tooltip
+                  cursor={{ strokeDasharray: '3 3' }}
+                  formatter={(value, type) =>
+                    type === 'date'
+                      ? moment(value).format('MM/DD HH:mm')
+                      : toComma(value)
+                  }
+                />
+                <Scatter name="A school" data={data} fill="#8884d8" />
+              </ScatterChart>
+            </ResponsiveContainer>
+          </div>
         ),
       ],
       [
@@ -155,6 +184,6 @@ export default () => {
         ),
       ],
     ],
-    () => <div>There is no data</div>
+    () => <div>There is no data</div>,
   )
 }
