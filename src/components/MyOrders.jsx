9fc13d6524a496d1de6c46ebb514fa6d258c4255
keating <min9nim>
2021-12-06 09:44:26 +0900

    etc

--- src/components/MyOrders.jsx
+++ src/components/MyOrders.jsx
@@ -14,98 +14,100 @@ import { toComma } from '@madup-inc/utils'
 import useTradePrice from '../SWRs/useTradePrice'
 
 export default function MyOrders({ data, currencies, market, setMarket }) {
-  const {data: tradePrice} = useTradePrice(market)
+  const { data: tradePrice } = useTradePrice(market)
   const currentPrice = tradePrice?.trade_price
 
   const avgPrice = currencies.find(
     item => item.currency === market,
   ).avg_buy_price
 
-  const profit = Math.floor(((currentPrice - avgPrice) / avgPrice) * 10000) / 100
+  const profit =
+    Math.floor(((currentPrice - avgPrice) / avgPrice) * 10000) / 100
   return (
     <div style={{ height: '100vh', padding: 3 }}>
-      <div>
-      <select
-        value={market}
-        name="market"
-        onChange={e => {
-          setMarket(e.target.value)
-        }}
-      >
-        {currencies.map(item => {
-          return (
-            <option key={item.currency} value={item.currency}>
-              {item.unit_currency + '-' + item.currency}
-            </option>
-          )
-        })}
-      </select>
-        <span style={{color: profit > 0 ? 'red' : 'blue', marginLeft: 10}}>{profit} %</span>
-
-      </div>
-      <div style={{ height: 'calc(100vh - 20px)' }}>
-      <ResponsiveContainer width="100%" height="100%">
-        <ScatterChart
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 20,
-            left: 20,
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <select
+          value={market}
+          name="market"
+          onChange={e => {
+            setMarket(e.target.value)
           }}
         >
-          <CartesianGrid />
-          <XAxis
-            type="number"
-            dataKey="x"
-            name="date"
-            angle={10}
-            tickFormatter={value => moment(value).format('YY/MM/DD')}
-            domain={['auto', 'auto']}
-          />
-          <YAxis
-            type="number"
-            width={50}
-            dataKey="y"
-            name="price"
-            domain={['auto', 'auto']}
-            tickFormatter={value =>
-              value > 1000000
-                ? toComma(value / 1000000) + '백만'
-                : toComma(value)
-            }
-          />
-          <ZAxis
-            type="number"
-            dataKey="z"
-            domain={['auto', 'auto']}
-            range={[50, 500]}
-            name="volume"
-          />
-          <Tooltip
-            cursor={{ strokeDasharray: '3 3' }}
-            formatter={(value, type) =>
-              type === 'date'
-                ? moment(value).format('MM/DD HH:mm')
-                : toComma(value)
-            }
-          />
-          <Scatter name="A school" data={data} fill="#8884d8" />
-          {avgPrice && (
-            <ReferenceLine
-              y={avgPrice}
-              label={'매수평균: ' + toComma(Math.floor(avgPrice))}
-              stroke="#4cc9f0"
+          {currencies.map(item => {
+            return (
+              <option key={item.currency} value={item.currency}>
+                {item.unit_currency + '-' + item.currency}
+              </option>
+            )
+          })}
+        </select>
+        <span style={{ color: profit > 0 ? 'red' : 'blue', marginLeft: 10, fontWeight: 'bold' }}>
+          {profit}%
+        </span>
+      </div>
+      <div style={{ height: 'calc(100vh - 20px)' }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis
+              type="number"
+              dataKey="x"
+              name="date"
+              angle={10}
+              tickFormatter={value => moment(value).format('YY/MM/DD')}
+              domain={['auto', 'auto']}
             />
-          )}
-          {currentPrice && (
-            <ReferenceLine
-              y={currentPrice}
-              label={'현재가: ' + toComma(currentPrice)}
-              stroke="#ffadad"
+            <YAxis
+              type="number"
+              width={50}
+              dataKey="y"
+              name="price"
+              domain={['auto', 'auto']}
+              tickFormatter={value =>
+                value > 1000000
+                  ? toComma(value / 1000000) + '백만'
+                  : toComma(value)
+              }
             />
-          )}
-        </ScatterChart>
-      </ResponsiveContainer>
+            <ZAxis
+              type="number"
+              dataKey="z"
+              domain={['auto', 'auto']}
+              range={[50, 500]}
+              name="volume"
+            />
+            <Tooltip
+              cursor={{ strokeDasharray: '3 3' }}
+              formatter={(value, type) =>
+                type === 'date'
+                  ? moment(value).format('MM/DD HH:mm')
+                  : toComma(value)
+              }
+            />
+            <Scatter name="A school" data={data} fill="#8884d8" />
+            {avgPrice && (
+              <ReferenceLine
+                y={avgPrice}
+                label={'매수평균: ' + toComma(Math.floor(avgPrice))}
+                stroke="#4cc9f0"
+              />
+            )}
+            {currentPrice && (
+              <ReferenceLine
+                y={currentPrice}
+                label={'현재가: ' + toComma(currentPrice)}
+                stroke="#ffadad"
+              />
+            )}
+          </ScatterChart>
+        </ResponsiveContainer>
       </div>
     </div>
   )
