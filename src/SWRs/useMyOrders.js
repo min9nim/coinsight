d7d2660cda091b1ff2b20646fc2a4fe1caf9c1fa
keating <min9nim>
2021-12-07 07:31:34 +0900

    load more page

--- src/SWRs/useMyOrders.js
+++ src/SWRs/useMyOrders.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useLoading } from 'react-hook-loading'
 import { toast } from 'react-toastify'
 
-export default function useMyOrders({ market, accessKey, secretKey }){
+export default function useMyOrders({ market, accessKey, secretKey }) {
   const [, setLoading] = useLoading()
   const { data } = useSWR(
     !market || accessKey.length !== 40 || secretKey.length !== 40
@@ -12,17 +12,14 @@ export default function useMyOrders({ market, accessKey, secretKey }){
     async (market, accessKey, secretKey) => {
       setLoading(true)
       try {
-        const result = await axios
-          .get(process.env.REACT_APP_API_SERVER + `/my-orders`, {
-            params: {
-              accessKey,
-              secretKey,
-              // state: 'cancel',
-              orderBy: 'asc',
-              market: 'KRW-' + market,
-            },
-          })
-          .then(result => result.data)
+        const result = []
+        for (let page = 1, res; ; page++) {
+          res = await req({ accessKey, secretKey, market, page })
+          result.push(...res)
+          if(res.length < 100){
+            break
+          }
+        }
         return result
       } finally {
         setLoading(false)
@@ -37,4 +34,19 @@ export default function useMyOrders({ market, accessKey, secretKey }){
     },
   )
   return { data }
+}
+
+function req({ accessKey, secretKey, market, page }) {
+  return axios
+    .get(process.env.REACT_APP_API_SERVER + `/my-orders`, {
+      params: {
+        accessKey,
+        secretKey,
+        // state: 'cancel',
+        orderBy: 'asc',
+        market: 'KRW-' + market,
+        page,
+      },
+    })
+    .then(result => result.data)
 }
