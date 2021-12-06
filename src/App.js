43898f3217eea585f08e106d3829980889dfd122
keating <min9nim>
2021-12-07 07:16:56 +0900

    setting of toastify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import MyTrades from './pages/my-orders'
 import {LoadingProvider} from 'react-hook-loading'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <LoadingProvider>
       <MyTrades/>
+      <ToastContainer />
     </LoadingProvider>
   );
 }
