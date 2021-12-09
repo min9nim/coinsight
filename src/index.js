1c7bb15d34371f659dea6e642244dfba00df928f
keating <min9nim>
2021-12-10 07:22:02 +0900

    fix language

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import moment from 'moment'
 
-moment.locale('en')
+moment.locale('ko')
 
 ReactDOM.render(
   <React.StrictMode>
