8612fa6dd8c69fb9e1e84c9229e0815f42ba4a79
keating <min9nim>
2021-12-28 23:09:34 +0900

    add ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import {Suspense} from 'react'
 import MyTrades from './pages/my-orders'
-import { LoadingProvider } from 'react-hook-loading'
+import { LoadingProvider, DefaultLoading } from 'react-hook-loading'
 import { ToastContainer } from 'react-toastify'
+import ErrorBoundary from './ErrorBoundary'
 import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
+export default function App() {
   return (
-    <LoadingProvider>
-      <MyTrades />
-      <ToastContainer />
-    </LoadingProvider>
+    <ErrorBoundary>
+      <LoadingProvider>
+        <Suspense fallback={<DefaultLoading />}>
+          <MyTrades />
+        </Suspense>
+        <ToastContainer />
+      </LoadingProvider>
+    </ErrorBoundary>
   )
 }
 
-export default App
+
