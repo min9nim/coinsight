b68c92764e4f4c5b0dee299f83b9795d1d6aa907
Keating <min9nim@gmail.com>
2022-02-18 17:13:16 +0900

    🎨 Add or update UI/UX

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -1,6 +1,12 @@
 import { toComma } from '@madup-inc/utils'
 
 export default function Header2({ coin, currentPrice, avgPrice, profit }) {
+  const texts = [
+    `- 보유수량: ${coin.balance}`,
+    `- 평가금액: ${toComma(Math.floor(coin.balance * currentPrice))}원`,
+    `- 매수금액: ${toComma(Math.floor(coin.balance * avgPrice))}원`,
+    `- 평가손익: ${toComma(profit)}원`,
+  ]
   return (
     <div
       style={{
@@ -10,42 +16,18 @@ export default function Header2({ coin, currentPrice, avgPrice, profit }) {
         marginTop: 5,
       }}
     >
-      <span
-        style={{
-          color: 'grey',
-          marginLeft: 10,
-          fontWeight: 'bold',
-        }}
-      >
-        - 보유수량: {coin.balance}
-      </span>
-      <span
-        style={{
-          color: 'grey',
-          marginLeft: 10,
-          fontWeight: 'bold',
-        }}
-      >
-        - 평가금액: {toComma(Math.floor(coin.balance * currentPrice))}원
-      </span>
-      <span
-        style={{
-          color: 'grey',
-          marginLeft: 10,
-          fontWeight: 'bold',
-        }}
-      >
-        - 매수금액: {toComma(Math.floor(coin.balance * avgPrice))}원
-      </span>
-      <span
-        style={{
-          color: 'grey',
-          marginLeft: 10,
-          fontWeight: 'bold',
-        }}
-      >
-        - 평가손익: {toComma(profit)}원
-      </span>
+      {texts.map((text, idx) => (
+        <span
+          key={idx}
+          style={{
+            color: 'grey',
+            margin: '3px 5px',
+            fontWeight: 'bold',
+          }}
+        >
+          {text}
+        </span>
+      ))}
     </div>
   )
 }
