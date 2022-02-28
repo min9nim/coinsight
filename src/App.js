02aa43939bc5d8741d0f98b4b98db4cf2369daae
Keating <min9nim@gmail.com>
2022-03-01 00:12:25 +0900

    setting of react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import ErrorBoundary from './ErrorBoundary'
 import { Toaster } from 'react-hot-toast'
 import Login from './components/Login'
 
+import { BrowserRouter } from 'react-router-dom'
+
 export default function App() {
-  return (
-    <ErrorBoundary>
-      <LoadingProvider>
-        <Suspense fallback={<DefaultLoading />}>
-          {window.location.pathname === '/my-orders' ? <MyTrades /> : <Login />}
-        </Suspense>
-      </LoadingProvider>
-      <Toaster />
-    </ErrorBoundary>
-  )
+    return (
+        <ErrorBoundary>
+            <LoadingProvider>
+                <Suspense fallback={<DefaultLoading />}>
+                    <BrowserRouter>
+                        {window.location.pathname === '/my-orders' ? (
+                            <MyTrades />
+                        ) : (
+                            <Login />
+                        )}
+                    </BrowserRouter>
+                </Suspense>
+            </LoadingProvider>
+            <Toaster />
+        </ErrorBoundary>
+    )
 }
