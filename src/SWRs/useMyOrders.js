e539b1e8be07a4e1f6462dd20295938e307ed34c
keating <min9nim@gmail.com>
2022-05-06 07:18:28 +0900

    ⚡️ Improve performance

--- src/SWRs/useMyOrders.js
+++ src/SWRs/useMyOrders.js
@@ -2,51 +2,65 @@ import useSWR from 'swr'
 import axios from 'axios'
 import { useLoading } from 'react-hook-loading'
 import { toast } from 'react-hot-toast'
+import { always, identity } from 'ramda'
 
 export default function useMyOrders({ market, accessKey, secretKey }) {
-  const [, setLoading] = useLoading()
-  const { data } = useSWR(
-    !market || accessKey.length !== 40 || secretKey.length !== 40 || accessKey === secretKey
-      ? null
-      : [market, accessKey, secretKey],
-    async (market, accessKey, secretKey) => {
-      setLoading(true)
-      try {
-        const result = []
-        for (let page = 1, res; ; page++) {
-          res = await req({ accessKey, secretKey, market, page })
-          result.push(...res)
-          if(res.length < 100){
-            break
-          }
-        }
-        return result
-      } finally {
-        setLoading(false)
-      }
-    },
-    {
-      onError(err, key, config) {
-        toast.error(err.message, { position: 'top-right' })
-        localStorage.clear()
-        window.location.assign('/')
-      },
-    },
-  )
-  return { data }
+    const [, setLoading] = useLoading()
+    const { data } = useSWR(
+        !market ||
+            accessKey.length !== 40 ||
+            secretKey.length !== 40 ||
+            accessKey === secretKey
+            ? null
+            : [market, accessKey, secretKey],
+        async (market, accessKey, secretKey) => {
+            setLoading(true)
+            try {
+                // const result = []
+                // for (let page = 1, res; ; page++) {
+                //   res = await req({ accessKey, secretKey, market, page })
+                //   result.push(...res)
+                //   if(res.length < 100){
+                //     break
+                //   }
+                // }
+                // return result
+
+                const result = await Promise.all([
+                    req({ accessKey, secretKey, market, page: 1 }),
+                    req({ accessKey, secretKey, market, page: 2 }),
+                    req({ accessKey, secretKey, market, page: 3 }),
+                    req({ accessKey, secretKey, market, page: 4 }),
+                  req({ accessKey, secretKey, market, page: 5 }),
+
+                ])
+                return result.flatMap(identity)
+            } finally {
+                setLoading(false)
+            }
+        },
+        {
+            onError(err, key, config) {
+                toast.error(err.message, { position: 'top-right' })
+                localStorage.clear()
+                window.location.assign('/')
+            },
+        },
+    )
+    return { data }
 }
 
 function req({ accessKey, secretKey, market, page }) {
-  return axios
-    .get(process.env.REACT_APP_API_SERVER + `/my-orders`, {
-      params: {
-        accessKey,
-        secretKey,
-        // state: 'cancel',
-        orderBy: 'asc',
-        market: 'KRW-' + market,
-        page,
-      },
-    })
-    .then(result => result.data)
+    return axios
+        .get(process.env.REACT_APP_API_SERVER + `/my-orders`, {
+            params: {
+                accessKey,
+                secretKey,
+                // state: 'cancel',
+                orderBy: 'asc',
+                market: 'KRW-' + market,
+                page,
+            },
+        })
+        .then(result => result.data)
 }
