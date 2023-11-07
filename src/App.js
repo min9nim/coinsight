ca5cdd2c6079757699722402e47c7e3129e35ab2
keating <min9nim@gmail.com>
2023-11-07 10:06:41 +0900

    format

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import { Suspense } from 'react'
 import MyOrders from './pages/my-orders'
-import { DefaultLoading, LoadingProvider } from 'react-hook-loading'
 import ErrorBoundary from './ErrorBoundary'
 import { Toaster } from 'react-hot-toast'
 import Login from './components/Login'
-import {useSearchParams} from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 
 export default function App() {
-    const [searchParam] =useSearchParams()
+    const [searchParam] = useSearchParams()
     const theme = searchParam.get('theme') || 'dark'
     return (
         <ErrorBoundary>
-            <LoadingProvider>
-                <div className={theme}>
-                    <Suspense fallback={<DefaultLoading />}>
-                        {window.location.pathname === '/my-orders' ? (
-                            <MyOrders theme={theme}/>
-                        ) : (
-                            <Login />
-                        )}
-                    </Suspense>
-                </div>
-            </LoadingProvider>
+            <div className={theme}>
+                <Suspense fallback={<div>Loading..</div>}>
+                    {window.location.pathname === '/my-orders' ? (
+                        <MyOrders theme={theme} />
+                    ) : (
+                        <Login />
+                    )}
+                </Suspense>
+            </div>
             <Toaster />
         </ErrorBoundary>
     )
